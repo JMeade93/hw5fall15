67fbd3371d0dbf95de550d7ff45af493f012fc36
jmeade93 <jacob-meade@uiowa.edu>
2015-10-16 04:49:27 +0000

    finished homework 5

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -28,7 +28,7 @@ Given /^I am on the RottenPotatoes home page$/ do
    click_on "More about #{title}"
  end
 
- Then /^(?:|I )should see "([^"]*)"$/ do |text|
+ Then /^(?:|I )should see "([^"]*)$/ do |text|
     expect(page).to have_content(text)
  end
 
@@ -46,12 +46,12 @@ Given /^I am on the RottenPotatoes home page$/ do
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies have been added to RottenPotatoes:/ do |movies_table|
-  pending  # Remove this statement when you finish implementing the test step
   movies_table.hashes.each do |movie|
     # Each returned movie will be a hash representing one row of the movies_table
     # The keys will be the table headers and the values will be the row contents.
     # Entries can be directly to the database with ActiveRecord methods
     # Add the necessary Active Record call(s) to populate the database.
+    Movie.create(movie)
   end
 end
 
@@ -59,16 +59,45 @@ When /^I have opted to see movies rated: "(.*?)"$/ do |arg1|
   # HINT: use String#split to split up the rating_list, then
   # iterate over the ratings and check/uncheck the ratings
   # using the appropriate Capybara command(s)
-  pending  #remove this statement after implementing the test step
+  ratings_array = ["G", "PG", "PG-13", "R", "NC-17"]
+  ratings_array.reject! {|x| arg1.split(', ').include?(x)}
+  ratings_array.each do |no_rating|
+     uncheck('ratings_'+no_rating) 
+  end
+    arg1.split(', ').each do |rating|
+        check('ratings_'+rating)
+        click_button('Refresh')
+    end
 end
 
 Then /^I should see only movies rated: "(.*?)"$/ do |arg1|
-  pending  #remove this statement after implementing the test step
+  ratings_array = ["G", "PG", "PG-13", "R", "NC-17"]
+  ratings_array.reject! {|x| arg1.split(', ').include?(x)}
+  within_table('movies') do
+      ratings_array.each do |no_rating|
+          all("td").each do |td|
+            expect(td.text).to_not eq(no_rating)
+          end
+      end
+  end
+    
+  arg1.split(', ').each do |rating|
+    expect(page).to have_content(rating)
+  end
 end
 
 Then /^I should see all of the movies$/ do
-  pending  #remove this statement after implementing the test step
+  movies = Movie.all
+  movies.each do |movie|
+     expect(page).to have_content(movie.title) 
+  end
+  
 end
 
+When /^I have opted to see the movies sorted by "(.*?)"$/ do |filter|
+    click_link filter
+end
 
-
+Then /^I should see "(.*?)" before "(.*?)"$/ do |movie1, movie2|
+    expect(page.body).to match /#{movie1}.*#{movie2}/m 
+end
